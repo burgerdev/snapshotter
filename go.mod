faa3e9fedf563d81136002f4e16d7f41fd8ce964
Markus Rudy <mr@edgeless.systems>
2024-06-26 06:01:59 +0200

    native snapshotter with gRPC logging

---
+++ go.mod
@@ -0,0 +1,36 @@
+module github.com/burgerdev/snapshotter
+
+go 1.22.3
+
+require (
+	github.com/containerd/containerd/api v1.8.0-rc.2
+	github.com/containerd/containerd/v2 v2.0.0-rc.3
+	google.golang.org/grpc v1.64.0
+	google.golang.org/protobuf v1.34.1
+)
+
+require (
+	github.com/Microsoft/go-winio v0.6.2 // indirect
+	github.com/Microsoft/hcsshim v0.12.4 // indirect
+	github.com/containerd/cgroups/v3 v3.0.3 // indirect
+	github.com/containerd/continuity v0.4.3 // indirect
+	github.com/containerd/errdefs v0.1.0 // indirect
+	github.com/containerd/log v0.1.0 // indirect
+	github.com/containerd/ttrpc v1.2.4 // indirect
+	github.com/containerd/typeurl/v2 v2.1.1 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
+	github.com/google/go-cmp v0.6.0 // indirect
+	github.com/moby/sys/mountinfo v0.7.1 // indirect
+	github.com/opencontainers/go-digest v1.0.0 // indirect
+	github.com/opencontainers/image-spec v1.1.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/sirupsen/logrus v1.9.3 // indirect
+	go.etcd.io/bbolt v1.3.10 // indirect
+	go.opencensus.io v0.24.0 // indirect
+	golang.org/x/net v0.25.0 // indirect
+	golang.org/x/sync v0.7.0 // indirect
+	golang.org/x/sys v0.21.0 // indirect
+	golang.org/x/text v0.15.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20240415180920-8c6c420018be // indirect
+)
---
+++ main.go
@@ -0,0 +1,104 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"os/signal"
+
+	"google.golang.org/grpc"
+	"google.golang.org/protobuf/encoding/protojson"
+	"google.golang.org/protobuf/proto"
+
+	snapshotsapi "github.com/containerd/containerd/api/services/snapshots/v1"
+	"github.com/containerd/containerd/v2/contrib/snapshotservice"
+	"github.com/containerd/containerd/v2/plugins/snapshots/native"
+)
+
+var (
+	addr       = flag.String("addr", "/tmp/snapshotter.sock", "Unix domain socket to listen on")
+	root       = flag.String("root", "/tmp/snapshots", "storage directory")
+	intercepts = flag.String("intercepts", "/tmp/snapshotter-intercepts.json", "intercept log file")
+)
+
+type Message struct {
+	kind   string
+	method string
+	msg    any
+}
+
+func (m *Message) MarshalJSON() ([]byte, error) {
+	protoMessage, ok := m.msg.(proto.Message)
+	if !ok {
+		return nil, fmt.Errorf("message had unexpected type %T", m.msg)
+	}
+	msg, err := protojson.Marshal(protoMessage)
+	if err != nil {
+		return nil, fmt.Errorf("marshalling proto: %w", err)
+	}
+
+	return []byte(fmt.Sprintf(`{"method": %q, "type": %q, "message": %q}`, m.method, m.kind, string(msg))), nil
+}
+
+type LoggingInterceptor struct {
+	f       *os.File
+	encoder *json.Encoder
+}
+
+func New() (*LoggingInterceptor, error) {
+	f, err := os.OpenFile(*intercepts, os.O_RDWR|os.O_CREATE, 0o644)
+	if err != nil {
+		return nil, fmt.Errorf("opening file %q: %w", *intercepts, err)
+	}
+	enc := json.NewEncoder(f)
+
+	return &LoggingInterceptor{f: f, encoder: enc}, nil
+}
+
+func (li *LoggingInterceptor) Intercept(ctx context.Context, req any, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (resp any, err error) {
+	log.Println("intercepting")
+	reqMsg := &Message{kind: "request", method: info.FullMethod, msg: req}
+	must(li.encoder.Encode(reqMsg))
+	resp, err = handler(ctx, req)
+	respMsg := &Message{kind: "response", method: info.FullMethod, msg: resp}
+	must(li.encoder.Encode(respMsg))
+	return resp, err
+}
+
+func (li *LoggingInterceptor) Close() {
+	li.f.Close()
+}
+
+func main() {
+	interceptor, err := New()
+	must(err)
+
+	rpc := grpc.NewServer(grpc.ChainUnaryInterceptor(interceptor.Intercept))
+
+	snappy, err := native.NewSnapshotter(*root)
+	must(err)
+
+	service := snapshotservice.FromSnapshotter(snappy)
+	snapshotsapi.RegisterSnapshotsServer(rpc, service)
+
+	l, err := net.Listen("unix", *addr)
+	must(err)
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, os.Interrupt)
+
+	go rpc.Serve(l)
+
+	<-sigs
+	rpc.Stop()
+	os.Remove(*addr)
+}
+
+func must(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
